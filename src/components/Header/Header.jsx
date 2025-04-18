12c5eeeeaf311c0c9ac6a9445d3927fdee453fd2
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2025-04-18 16:35:43 -0300

    login-sreen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,59 @@
-import { NavLink } from "react-router-dom";
-import exameSus from '../../assets/exame-sus.jpeg';
+import { NavLink, useLocation } from "react-router-dom";
+import exameSus from "../../assets/image/exame-sus.png";
 import "./Header.css";
 
 function Header() {
-    
+  const location = useLocation();
+  const isLoginPage =
+    location.pathname === "/" || location.pathname === "/login";
+
   return (
     <header className="container">
-      <div className="container-logo"><img src={exameSus} alt="Logo SUS"  width={100} height={100}/></div>
+      <div className="container-logo">
+        <img src={exameSus} alt="Logo SUS" width={270} height={60} />
+      </div>
 
       <nav>
         <ul className="container-nav">
+          {!isLoginPage && (
+            <li>
+              <NavLink
+                to={"/login"}
+                className={({ isActive }) => (isActive ? "selected" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+          )}
+
           <li>
             <NavLink
-              to={"/home"}
+              to={"/produto/home"}
               className={({ isActive }) => (isActive ? "selected" : "")}
             >
-              Home
+              Sabe como funciona a FILA?
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to={"/produto/cadastrar"}
-              className={({ isActive }) => isActive ? "selected" : ""}
-            >
-              Metodologias de prioridades
-            </NavLink>
-          </li>
+
           <li>
             <NavLink
               to={"/produtos"}
-              className={({ isActive }) => isActive ? "selected" : ""}
+              className={({ isActive }) => (isActive ? "selected" : "")}
             >
               Histórico de pesquisa
             </NavLink>
           </li>
-          <li>
-            <NavLink
-              to={"/datacep"}
-              className={({ isActive }) => isActive ? "selected" : ""}
-            >
-              Sair
-            </NavLink>
-          </li>
+
+          {!isLoginPage && (
+            <li>
+              <NavLink
+                to={"/datacep"}
+                className={({ isActive }) => (isActive ? "selected" : "")}
+              >
+                Sair
+              </NavLink>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
