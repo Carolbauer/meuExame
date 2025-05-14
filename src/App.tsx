2b26d44d92b2d84634103550286b8344f0bfeaa0
carolbauer <99449590+Carolbauer@users.noreply.github.com>
2025-05-14 20:29:26 -0300

    add button exams details

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import Exams from "./pages/Exams/ExamsDetailsPage";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         <Route index element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/exame/:id" element={<Exams />} />
+
+        
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </BrowserRouter>
